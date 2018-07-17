caa5f9ba127fa0da0375979944c1837af5290ae2
Justin Donaldson <jdonaldson@gmail.com>
2018-07-17 12:28:47 -0700

    tweak install script for languagserver

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cd haxe-languageserver
 haxelib git haxe-hxparser https://github.com/vshaxe/haxe-hxparser --always
+haxelib git vshaxe-build https://github.com/vshaxe/vshaxe-build.git --always
 haxelib install hxnodejs --always
-haxe build.hxml
+echo y | haxelib run vshaxe-build -t language-server -m both
